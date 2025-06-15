e7d5200ce3c04d530af5dd2561d52a37f057185c
Developerkubilay <kubiss52@yahoo.com>
2025-06-15 14:38:07 +0300

    feat: Update AI processing configuration; increase max retries and adjust interval handling

--- modules/ai.js
+++ modules/ai.js
@@ -3,41 +3,55 @@ const ai = new GoogleGenAI({ apiKey: process.env.Gemini_API_KEY });
 const model = require('../config.json').model
 const c = require('ansi-colors');
 
-const MAX_RETRIES = 3;
+const MAX_RETRIES = 10;
 const RETRY_DELAY = 5000;
+const awaits = [];
+
+setInterval(() => {
+  if (awaits.length > 0) {
+    const { data, resolve, reject } = awaits.shift();
+    generateText(data, resolve, reject);
+  }
+}, config.AI_INT)
 
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-module.exports = async function(data) {
+module.exports = async function (data) {
   console.log(c.green("🔍 AI ile içerik oluşturuluyor..."));
-  let retries = 0;
-  
-  while (retries < MAX_RETRIES) {
-    try {
-      const generationConfig = {
-        maxOutputTokens: 8192
-      };
+  return await new Promise((res, reject) => {
+    awaits.push({ data, resolve: res, reject });
+  });
+}
 
-      const response = await ai.models.generateContent({
-        model: model,
-        contents: [{
-          text: data
-        }],
-        generationConfig,
-        responseMimeType: 'text/plain'
-      });
-      return response.text.replace(/[()]/g, '');
-    } catch (err) {
-      retries++;
-      console.error(c.red(`🚫 AI Hatası (Deneme ${retries}/${MAX_RETRIES}):`, err));
-      
-      if (retries >= MAX_RETRIES) {
-        console.error(c.bgRed.white("❌ Maksimum deneme sayısına ulaşıldı, işlem başarısız!"));
-        throw err;
+
+async function generateText(data, resolve, reject) { 
+    let retries = 0;
+    while (retries < MAX_RETRIES) {
+      try {
+        const generationConfig = {
+          maxOutputTokens: 8192
+        };
+
+        const response = await ai.models.generateContent({
+          model: model,
+          contents: [{
+            text: data
+          }],
+          generationConfig,
+          responseMimeType: 'text/plain'
+        });
+        resolve(response.text.replace(/[()]/g, ''));
+      } catch (err) {
+        retries++;
+        console.error(c.red(`🚫 AI Hatası (Deneme ${retries}/${MAX_RETRIES}):`, err));
+
+        if (retries >= MAX_RETRIES) {
+          console.error(c.bgRed.white("❌ Maksimum deneme sayısına ulaşıldı, işlem başarısız!"));
+          reject(err);
+        }
+
+        console.log(c.blue(`⏳ ${RETRY_DELAY / 1000} saniye sonra tekrar deneniyor...`));
+        await sleep(RETRY_DELAY);
       }
-      
-      console.log(c.blue(`⏳ ${RETRY_DELAY/1000} saniye sonra tekrar deneniyor...`));
-      await sleep(RETRY_DELAY);
     }
-  }
 }
