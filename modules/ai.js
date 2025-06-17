c2d1ee6f868cd63f775aafa9362b7efdf7346dfe
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2025-06-17 19:49:22 +0300

    feat: Remove AI_RETRY_DELAY from configuration and refactor retry logic in AI module

--- modules/ai.js
+++ modules/ai.js
@@ -5,25 +5,27 @@ const FixeerAi = new GoogleGenAI({ apiKey: process.env.Gemini_API_KEY_2 });
 const config = require('../config.json')
 const c = require('ansi-colors');
 
-const MAX_RETRIES = 15;
-const RETRY_DELAY = config.AI_RETRY_DELAY
 const awaits = [];
 var errscount = {};
 var datenw = Date.now();
 setInterval(() => {
   datenw = Date.now();
-},1000)
+}, 1000)
 
 setInterval(() => {
-  if (Object.keys(errscount).length === 0 && awaits.length > 0) {
-    const { data, resolve, reject } = awaits.shift();
-    generateText(data, resolve, reject);
-  } else if (Object.keys(errscount).length > 0) {
+  if (Object.keys(errscount).length > 0) {
     console.log(c.yellow("⚠️ Bazı yapay zeka hataları nedeniyle bekleyen işlemler var!"));
+    for (const key in errscount) {
+      const { data, resolve, reject, starttime } = errscount[key];
+      delete errscount[key];
+      generateText(FixeerAi, starttime, data, resolve, reject);
+    }
+  }
+  else if (Object.keys(errscount).length === 0 && awaits.length > 0) {
+    const { data, resolve, reject } = awaits.shift();
+    generateText(datenw, data, resolve, reject);
   }
 }, config.AI_INT)
-
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 module.exports = async function (data) {
   return await new Promise((res, reject) => {
@@ -32,63 +34,35 @@ module.exports = async function (data) {
 }
 
 var lastai = true;
-function giveAi(){
+function giveAi() {
   lastai = !lastai;
   if (lastai) return ai;
   return Alternative_ai;
 }
 
 
-async function generateText(data, resolve, reject) {
-  let retries = 0;
-  let igoterr = false;
-  const starttime = datenw
+async function generateText(starttime, data, resolve, reject) {
+  try {
+    console.log(c.green(`🔍 ${Object.keys(errscount).length > 0 ? "Hatayı düzeltiliyor" : "AI ile içerik oluşturuluyor"} ... ${awaits.length} kadar bekleyen var`));
+    const generationConfig = {
+      maxOutputTokens: 8192
+    };
 
-  async function Asyncsleep(){
-    igoterr = false;
-    await sleep(RETRY_DELAY);
-    if(igoterr) console.log(c.yellow("⏳ Ai hala hatayı çözemedi, bekleniyor..."));
-    else {
-      delete errscount[starttime]
-      console.log(
-         c.greenBright("✨ AI hatası çözüldü, işlem devam ediyor! 💯 Hala şu kadar hatada bekleyen var: "),
-         Object.keys(errscount).length
-        );
-    }
-  }
-
-  while (retries < MAX_RETRIES) {
-    try {
-      console.log(c.green(`🔍 AI ile içerik oluşturuluyor... ${awaits.length} kadar bekleyen var`));
-      const generationConfig = {
-        maxOutputTokens: 8192
-      };
-
-      if(igoterr) Asyncsleep();
-      const response = await (Object.keys(errscount).length > 0 ? FixeerAi : giveAi()).models.generateContent({
-        model: config.model,
-        contents: [{
-          text: data
-        }],
-        generationConfig,
-        responseMimeType: 'text/plain'
-      });
-      const size = Math.floor(response.text.length/1024)
-      console.log(c.green(`✅ AI içeriği başarıyla oluşturuldu! ${(Date.now()-starttime)/1000}s ${size} KB`));
-      resolve(response.text.replace(/[()]/g, ''));
-    } catch (err) {
-      errscount[starttime] = true;
-      retries++;
-      igoterr = true;
-      console.error(c.red(`🚫 AI Hatası (Deneme ${retries}/${MAX_RETRIES}):`, err));
-
-      if (retries >= MAX_RETRIES) {
-        console.error(c.bgRed.white("❌ Maksimum deneme sayısına ulaşıldı, işlem başarısız!"));
-        reject(err);
-      }
-
-      console.log(c.blue(`⏳ ${RETRY_DELAY / 1000} saniye sonra tekrar deneniyor...`));
-      await sleep(RETRY_DELAY);
-    }
+    if (igoterr) Asyncsleep();
+    const response = await (Object.keys(errscount).length > 0 ? FixeerAi : giveAi()).models.generateContent({
+      model: config.model,
+      contents: [{
+        text: data
+      }],
+      generationConfig,
+      responseMimeType: 'text/plain'
+    });
+    const size = Math.floor(response.text.length / 1024)
+    console.log(c.green(`✅ AI içeriği başarıyla oluşturuldu! ${(Date.now() - starttime) / 1000}s ${size} KB`));
+    resolve(response.text.replace(/[()]/g, ''));
+  } catch (err) {
+    errscount[starttime] = {
+      data, resolve, reject, starttime
+    };
   }
 }
